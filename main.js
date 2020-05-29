a155f728376d6f452201ad9842471a4cc564d96b
Mark Hajnal <mhajnal@sympatico.ca>
2020-05-29 17:29:46 -0400

    clicked buttons show on display, C clears display'

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
 
+let nums = document.getElementsByClassName('num');
+let nmbr = [];
+for (let i = 0; i < nums.length; i++) {
+  nmbr.push(nums[i].textContent);
+  nums[i].setAttribute('id', `btn${nmbr[i]}`);
+}
+
+let disp = document.querySelector('#disp');
+/*
+let dispNum = [];
+let nmbrBtns = document.querySelectorAll('.num');
+nmbrBtns.forEach(btn => btn.addEventListener('click', event => {
+  dispNum.push(event.target.textContent);
+  //dspN=dispNum.join('');
+  disp.textContent = dispNum.join(''); 
+}));
+*/
+
+let dispItems = [];
+let btns = document.querySelectorAll('button');
+btns.forEach(btn => btn.addEventListener('click', event => {
+   if (event.target.textContent === 'C') {
+      dispItems = [];
+      disp.textContent = 0; //0 stays when other keys are pushed
+    } else {
+      dispItems.push(event.target.textContent);
+      disp.textContent = dispItems.join('');
+    }
+    //if textContent == '+', store everything before + in a variable
+  }));
+
 
 
 function add(a,b){
