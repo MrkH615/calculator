63455d00bc41fca7a5c8a89f922cc31f688e69fb
Mark Hajnal <mhajnal@sympatico.ca>
2020-07-04 22:04:10 -0400

    Calculator supports order of operations, can use result for new calculatrion

--- main-3.js
+++ main-3.js
@@ -50,14 +50,20 @@ function putOnDisplay(){
             let numsAndSigns =  splitAtSigns(dispItems);
             let nums = numsAndSigns['nums'];
             let signs = numsAndSigns['signs'];
-            let result = nums[0];
+            /*let result = nums[0];
 
             for (let i = 0; i < nums.length-i; i++) {
                 result = Calculate[signs[0]](result, nums[i+1]);
             }
-
                 console.log(result);
-                DISP.textContent = result; 
+                DISP.textContent = result; */
+
+            let result = getResult(nums,signs);
+            dispItems=[]
+            dispItems.push(result);
+            console.log(dispItems);
+            DISP.textContent = dispItems;
+
             // equals ends
         } 
 
@@ -69,8 +75,8 @@ function putOnDisplay(){
 
 putOnDisplay();
 
-    function splitAtSigns(arr) {
-        arr.pop(); //removes equals sign
+function splitAtSigns(arr) {
+  arr.pop(); //removes equals sign
         let arrStr = arr.join('');
         let signs = Array.from(arrStr.matchAll(OPERATORS), sgn => sgn[0]); 
         let nums = arrStr.split(OPERATORS).map(nmbr => Number(nmbr)); //change nums from strings into nums
@@ -78,8 +84,58 @@ putOnDisplay();
         numsAndSigns['nums'] = nums;
         numsAndSigns['signs'] = signs;
 
-        return numsAndSigns;         
+    return numsAndSigns;         
     }
+
+function getResult(nums, signs){
+      let numsToDrop;
+      while(nums.length > 1) {
+      
+        for (let i=0; i <nums.length; i++)  {
+        
+          switch(signs[i]) {
+            case '×': 
+             
+              nums.splice(i,2, Calculate[signs[i]](nums[i],nums[i+1]));
+              signs.splice(i,1);
+              i=i-2;
+            break;
+      
+            case '÷':
+         
+              nums.splice(i,2, Calculate[signs[i]](nums[i], nums[i+1]));
+               signs.splice(i,1);
+               i=i-2;
+            break;
+          } 
+        
+        }
+      
+        for (let j=0; j<nums.length; ++j) {
+       
+          switch(signs[j]) {
+          
+            case '+':
+             
+              nums.splice(j, 2, Calculate[signs[j]](nums[j],nums[j+1]));
+              signs.splice(j,1);
+              j=j-2;
+            break;
+      
+            case '-':
+      
+              nums.splice(j, 2,Calculate[signs[j]](nums[j], nums[j+1]));
+              signs.splice(j,1);
+              j=j-2;
+           
+            break;  
+           }
+        } 
+        let result = nums;
+        console.log('Final answer '+ result); 
+        return result.toString();
+      }
+}
 
 
 
