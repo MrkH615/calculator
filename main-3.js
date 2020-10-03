b074d2a976d311a0ad1f0dce357eed90001aee71
Mark Hajnal <mhajnal@sympatico.ca>
2020-10-02 21:52:15 -0400

    Changed backspace character, deleted commented code and console logs

--- main-3.js
+++ main-3.js
@@ -1,4 +1,4 @@
-//const OPERATORS = /[–×÷+*\/-]/g;
+
 const OPERATORS = /[×÷+*\/-]/g;
 const Neg = /[–]/g;
 
@@ -30,16 +30,11 @@ function putOnDisplay(){
     let btns = document.querySelectorAll('button');
     btns.forEach(btn => btn.addEventListener('click', event => {
 
-//console.log(event.target.textContent);
-
-
         if(event.target.className !== 'correct') {
 
           if (dispItems[0] === '0' && dispItems[1] !== '.') {
             dispItems.shift();
           }
-
-            //disable operator buttons after operator button clicked
             
             //if (event.target.textContent.match(OPERATORS)){  //works 
             if (event.target.className == 'operator') { // works
@@ -52,14 +47,6 @@ function putOnDisplay(){
               Operator.forEach(operator => {
                 operator.disabled = false});
             }
-
-            /*if (event.target.textContent === "⁺⁄₋") { //textContent still displayed later
-              dispItems.push('-');
-              //dispItems.pop();
-            }*/
-            
-            
-            //enable operator buttons after number button clicked
           
            if (event.target.textContent === '.') {
                Decimal.disabled = true;
@@ -71,27 +58,15 @@ function putOnDisplay(){
 
 
            event.target.textContent === "⁺⁄₋" ? dispItems.push('–') : dispItems.push(event.target.textContent);  
-          //NOT A  MINUS SIGN pushed val is en dash U+2013  &ndash; &#x2013; &#8211;
+          //en dash, not minus sign   &#x2013; &#8211;
            
-  
-           //dispItems.push(event.target.textContent);
-            
-           if (event.target.textContent === '0' && dispItems[dispItems.indexOf('0') -1] === '÷' || dispItems[dispItems.indexOf('0')] && dispItems[dispItems.indexOf('0')-1] === '÷') {
-            dispItems = ['ERROR!'];  //if C clicked here other error messages are still displayed
+           if (event.target.textContent === '0' && dispItems[dispItems.indexOf('0') -1] === '÷' 
+           || dispItems[dispItems.indexOf('0')] && dispItems[dispItems.indexOf('0')-1] === '÷') {  
              Operator.forEach(btn => btn.disabled = true); 
              NUM.forEach(btn => btn.disabled = true); 
-             if (event.target.textContent === 'C') {
-               dispItems = clear();
-               DISP.textContent = dispItems.join(''); //displays 0, but then setTimeout()s run
-               clearTimeout(); //doesn't make a difference here
-             } //end of clear
-             setTimeout(() => {  //if C clicked here other error messages are still displayed
-               DISP.textContent = ['Division by 0'];
-             }, 2000);
-             setTimeout(() => {
-               DISP.textContent = ['Click C to clear'];
-             }, 4000); //need to wait until here for C to work
-           }
+             dispItems = ['ERROR! C = clear'];
+    
+           } 
 
             DISP.textContent = dispItems.join('');
 
@@ -112,32 +87,17 @@ function putOnDisplay(){
             let numsAndSigns =  splitAtSigns(dispItems);
             let nums = numsAndSigns['nums'];
             let signs = numsAndSigns['signs'];
-            /*let result = nums[0];
-
-            for (let i = 0; i < nums.length-i; i++) {
-                result = Calculate[signs[0]](result, nums[i+1]);
-            }
-                console.log(result);
-                DISP.textContent = result; */
+           
 
             let result = getResult(nums,signs);//.split('');
             console.log('result is a '+typeof result); //with split -> object
             dispItems=result; //seems to work
-            //dispItems.push(result);
+            
             console.log(dispItems+' '+typeof(dispItems));//object
-            //console.log(typeof dispItems[0]);//object-->strange
-            //console.log(dispItems.join(''));
             DISP.textContent = dispItems.join('');
 
             // equals ends
-        } //code for backspace here?
-/*
-        Backspace.addEventListener('click', (e) => { //←
-          //Uncaught TypeError: can't access property "addEventListener", Backspace is null
-          dispItems.pop(); 
-          console.log(dispItems); 
-          DISP.textContent = dispItems.join('');
-        }); //doesn't work*/
+        } 
 
     }));
 
@@ -146,48 +106,39 @@ function putOnDisplay(){
 putOnDisplay();
 
 function splitAtSigns(arr) {
-        console.log('dispArr in splitAtSigns = ' + arr);
-        arr.pop(); //removes equals sign
-        let arrStr = arr.join('');
-        let signs = Array.from(arrStr.matchAll(OPERATORS), sgn => sgn[0]); 
-        console.log('signs ' + signs);
         
-        console.log('arrStr.split(OPERATORS) = ' + arrStr.split(OPERATORS));//.replace(/^,/,'')
-        console.log(typeof arrStr.split(OPERATORS));//object
-      
-        //nums = arrStr.split(OPERATORS).map(nmbr => Number(nmbr)); //change nums from strings into nums
-        //let nums = arrStr.split(OPERATORS);//.map(nmbr => Number(nmbr)); //change nums from strings into nums
-        let nums = arrStr.split(OPERATORS)
-                         .map(num => num.replace(Neg, '-'))
-                         .map(num => Number(num));
-        console.log('nums '+ nums); //undefined
-        let numsAndSigns = {};
-        numsAndSigns['nums'] = nums; // 
-        numsAndSigns['signs'] = signs;
-
-    return numsAndSigns;         
-    }
+  arr.pop(); //removes equals sign
+  let arrStr = arr.join('');
+  let signs = Array.from(arrStr.matchAll(OPERATORS), sgn => sgn[0]); 
+  let nums = arrStr.split(OPERATORS)
+                   .map(num => num.replace(Neg, '-'))
+                   .map(num => Number(num));
+  let numsAndSigns = {};
+  numsAndSigns['nums'] = nums; 
+  numsAndSigns['signs'] = signs;
+  return numsAndSigns;         
+}
 
 function getResult(nums, signs){
-      let numsToDrop;
-      while(nums.length > 1) {
+  let numsToDrop;
+  while(nums.length > 1) {
 
-        for (let i=0; i <nums.length; i++)  {
+    for (let i=0; i <nums.length; i++)  {
         
-          switch(signs[i]) {
-            case '×': 
+      switch(signs[i]) {
+        case '×': 
              
-              nums.splice(i,2, Calculate[signs[i]](nums[i],nums[i+1]));
-              signs.splice(i,1);
-              i=i-2;
-            break;
+          nums.splice(i,2, Calculate[signs[i]](nums[i],nums[i+1]));
+          signs.splice(i,1);
+          i=i-2;
+        break;
       
-            case '÷':
+        case '÷':
          
-              nums.splice(i,2, Calculate[signs[i]](nums[i], nums[i+1]));
-               signs.splice(i,1);
-               i=i-2;
-            break;
+          nums.splice(i,2, Calculate[signs[i]](nums[i], nums[i+1]));
+          signs.splice(i,1);
+          i=i-2;
+        break;
           } 
         
         }
@@ -220,14 +171,17 @@ function getResult(nums, signs){
 
 
 function clear() {
+  console.log('clear()');
   dispItems = ['0'];
   DISP.textContent = dispItems.join('');
-  btns.forEach(btn => btn.disabled = false);
+  //btns.forEach(btn => btn.disabled = false);
+  Operator.forEach(btn => btn.disabled = false);
+  NUM.forEach(btn => btn.disabled = false);
+  //DISP.classList.remove('error');
+  console.log(DISP.className);
   return dispItems;
 }
 
-function sparseToDense (arr) {
-  return arr.filter(()=>true);
-}
+
 
 
